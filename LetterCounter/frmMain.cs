6b0c1f7af47564df619a930669647e22c5aafc2b
Ross Ylitalo <ross.ylitalo@gmail.com>
2018-01-13 10:55:29 -0500

    Initial Commit, push to local and github.

--- LetterCounter/frmMain.cs
+++ LetterCounter/frmMain.cs
@@ -10,11 +10,20 @@ using System.Windows.Forms;
 
 namespace LetterCounter
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void cmdOpenTextFile_Click(object sender, EventArgs e)
+        {
+            if (openTextFile.ShowDialog(this) == DialogResult.OK)
+            {
+                
+
+            }
         }
     }
 }
